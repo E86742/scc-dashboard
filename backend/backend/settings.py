789681b0a500d2a619cb6452a34450a3d700f9d1
zKjet <100480520+zkjet@users.noreply.github.com>
2022-10-18 21:43:03 -0600

    db configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
