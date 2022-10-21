90c5c97dd7df0bb09f4194bf35d5eca8b22356d9
zKjet <100480520+zkjet@users.noreply.github.com>
2022-10-21 07:15:35 -0600

    credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
